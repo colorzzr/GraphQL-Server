2d7026479ecc9f8c462a94ac276b8dc3ba6c5ad9
colorzzr <colorzzr@gmail.com>
2019-06-21 12:28:44 -0400

    init project with simple ql connection

--- src/index.js
+++ src/index.js
@@ -1,13 +1,64 @@
-import 'dotenv/config';
+import express from 'express';
+import { ApolloServer, gql } from 'apollo-server-express';
+import cors from 'cors';
 
-const userCredentials = { firstname: 'Robin' };
-const userDetails = { nationality: 'German' };
 
-const user = {
-  ...userCredentials,
-  ...userDetails,
+const app = express();
+
+// allow the cross domain access
+app.use(cors());
+
+// define the database schema
+const schema = gql`
+  type Query {
+  	users: [User!]
+    me: User
+    user(id: ID!): User
+  }
+
+  type User {
+  	id: ID!
+    username: String!
+  }
+`;
+
+let users = {
+  1: {
+    id: '1',
+    username: 'Robin Wieruch',
+  },
+  2: {
+    id: '2',
+    username: 'Dave Davids',
+  },
 };
 
-console.log(user);
+const me = users[1];
 
-console.log(process.env.SOME_ENV_VARIABLE);
+// give the hleper function to do the query
+const resolvers = {
+  Query: {
+  	users: () => {
+      return Object.values(users);
+    },
+    me: () => {
+    	return me;
+    },
+    // input is {id:val} we only want val
+    user: (parent, { id }) => {
+    	console.log(id);
+    	return users[id];
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers,
+});
+
+server.applyMiddleware({ app, path: '/graphql' });
+
+app.listen({ port: 8000 }, () => {
+  console.log('Apollo Server on http://localhost:8000/graphql');
+});
