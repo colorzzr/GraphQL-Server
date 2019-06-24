4536d635ddb399db4d72fd42c18c00dea9de1fd8
colorzzr <colorzzr@gmail.com>
2019-06-23 20:16:45 -0700

    start to configre the pg

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { ApolloServer, gql } from 'apollo-server-express';
 import cors from 'cors';
 
-import models from './models';
+import models, { sequelize } from './models';
 import resolvers from './resolvers';
 import schema from './schema';
 
@@ -26,6 +26,8 @@ const server = new ApolloServer({
 
 server.applyMiddleware({ app, path: '/graphql' });
 
-app.listen({ port: 8000 }, () => {
-  console.log('Apollo Server on http://localhost:8000/graphql');
+sequelize.sync().then(async () => {
+  app.listen({ port: 8000 }, () => {
+    console.log('Apollo Server on http://localhost:8000/graphql');
+  });
 });
